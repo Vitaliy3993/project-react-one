72dbe24fd3d689c99bef91cfa98bf80fd49c9581
vitaliy3993 <kadelchukkp@gmail.com>
2022-04-04 14:37:02 +0300

    5

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 function App() {
 
   const data = [
-    {name: 'John C.', salary: 800, increase: false},
-    {name: 'Alex V.', salary: 4000, increase: true},
-    {name: 'Carl O.', salary: 15000, increase: false},
+    {name: 'John C.', salary: 800, increase: false, id: 1},
+    {name: 'Alex V.', salary: 4000, increase: true, id: 2},
+    {name: 'Carl O.', salary: 15000, increase: false, id: 3},
   ]
 
 
