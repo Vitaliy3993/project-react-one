0e6226a1b641d3675a03edfab0c7b1e1567b2ca0
vitaliy3993 <kadelchukkp@gmail.com>
2022-04-04 10:23:53 +0300

    props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,14 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+  const data = [
+    {name: 'John C.', salary: 800, increase: false},
+    {name: 'Alex V.', salary: 4000, increase: true},
+    {name: 'Carl O.', salary: 15000, increase: false},
+  ]
+
+
   return (
     <div className="app">
         <AppInfo/>
@@ -16,7 +24,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );
